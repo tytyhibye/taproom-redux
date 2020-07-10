708b3a04805e16d89fa4056d09f2d93a969087b9
Tyler Bates <Fondlytyler@gmail.com>
2020-07-10 08:49:37 -0700

    build out action commands and assign them to variables.

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,4 @@
+export const ADD_BEER = 'ADD_BEER';
+export const DELETE_BEER = 'DELETE_BEER';
+export const TOGGLE_FORM = 'TOGGLE_FORM';
+export const UPDATE_TIME = 'UPDATE_TIME';
---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+import * as c from './../actions/ActionTypes';
+
+export const deleteBeer = id => ({
+  type: c.DELETE_TICKET,
+  id
+});
+
+export const toggleForm = () => ({
+  type: c.TOGGLE_FORM
+});
+
+export const addBeer = (beer) => {
+  const { name, brand, price, abv, description, timeTapped } = beer;
+  return {
+    type: c.ADD_BEER,
+    name: name,
+    brand: brand,
+    price: price,
+    abv: abv,
+    description: description,
+    timeTapped: timeTapped
+  }
+}
+
+export const updateTime = (id, formattedWaitTime) => ({
+  type: c.UPDATE_TIME,
+  id: id,
+  formattedWaitTime: formattedWaitTime
+});
