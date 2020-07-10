ec45de23014089f294b2c8b26b10dbd83df20b98
Tyler Bates <Fondlytyler@gmail.com>
2020-07-10 09:34:06 -0700

    build out reducer test actions and first test.

--- src/__tests__/reducers/beer-list-reducer.test.js
+++ src/__tests__/reducers/beer-list-reducer.test.js
@@ -5,7 +5,7 @@ describe("beerListReducer", () => {
 
   let action;
 
-  const beerDetails ={
+  const beerDetail ={
     name: "RPM",
     brand: "Boneyard",
     price: "7",
@@ -16,7 +16,7 @@ describe("beerListReducer", () => {
   };
 })
 
-const updatedBeerDetails = {
+const updatedBeerDetail = {
   name: "RPM",
   brand: "Boneyard",
   price: "7",
@@ -25,3 +25,45 @@ const updatedBeerDetails = {
   timeTapped: 0,
   id: 1
 };
+
+const currentState = {
+  1: {
+    name: "RPM",
+    brand: "Boneyard",
+    price: "7",
+    abv: "6",
+    description: "perfectly dank",
+    timeTapped: 0,
+    id: 1
+  },
+  2: {
+  name: "Irish Death",
+  brand: "Ironhorse",
+  price: "7",
+  abv: "8",
+  description: "body of an ale, flavor of a stout",
+  timeTapped: 0,
+  id: 1
+  },
+};
+
+test('Should add a formatted shelf life to a tapped beer', () => {
+  const { name, brand, price, abv, description, timeTapped, id} = beerDetail;
+  action = {
+    type: c.UPDATE_TIME,
+    formattedWaitTime: '4 minutes',
+    id: id
+  };
+  expect(ticketListReducer({ [id] : beerDetail }, action)).toEqual({
+    [id] : {
+      name: name,
+      brand: brand,
+      price: price,
+      abv: abv,
+      description: description,
+      timeTapped: timeTapped,
+      id: id,
+      formattedWaitTime: '4 minutes'
+    }
+  });
+});
