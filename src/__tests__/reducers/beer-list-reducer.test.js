94ed9631452ea94ac9c29b476218c128d088a376
Tyler Bates <Fondlytyler@gmail.com>
2020-07-10 09:21:22 -0700

    write framework for reducer tests.

--- src/__tests__/reducers/beer-list-reducer.test.js
+++ src/__tests__/reducers/beer-list-reducer.test.js
@@ -1,3 +1,27 @@
 import beerListReducer from "../../reducers/beer-list-reducer";
 import Moment from 'moment';
 
+describe("beerListReducer", () => {
+
+  let action;
+
+  const beerDetails ={
+    name: "RPM",
+    brand: "Boneyard",
+    price: "7",
+    abv: "6",
+    description: "perfectly dank",
+    timeTapped: 0,
+    id: 1
+  };
+})
+
+const updatedBeerDetails = {
+  name: "RPM",
+  brand: "Boneyard",
+  price: "7",
+  abv: "6",
+  description: "a balanced blend of citrus and pine",
+  timeTapped: 0,
+  id: 1
+};
